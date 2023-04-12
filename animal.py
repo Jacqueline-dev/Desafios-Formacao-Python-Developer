0a4b1b7bbe25f0d7e6727b27d5a0c73a55b1e2dc
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2023-04-12 18:49:14 -0300

    desafio animal

---
+++ animal.py
@@ -0,0 +1,63 @@
+A = input()
+B = input()
+C = input()
+
+if A == 'vertebrado' and B == 'ave' and C == 'carnivoro':
+    resposta = 'aguia'
+
+if A == 'vertebrado' and B == 'ave' and C == 'onivoro':
+    resposta = 'pomba'
+
+if A == 'vertebrado' and B == 'mamifero' and C == 'onivoro':
+    resposta = 'homem'
+
+if A == 'vertebrado' and B == 'mamifero' and C == 'herbivoro':
+    resposta = 'vaca'
+
+if A == 'invertebrado' and B == 'inseto' and C == 'hematofago':
+    resposta = 'pulga'
+
+if A == 'invertebrado' and B == 'inseto' and C == 'herbivoro':
+    resposta = 'lagarta'
+
+if A == 'invertebrado' and B == 'anelideo' and C == 'hematofago':
+    resposta = 'sanguessuga'
+
+if A == 'invertebrado' and B == 'anelideo' and C == 'onivoro':
+    resposta = 'minhoca'
+
+print(resposta)
+
+
+'''
+
+x = input()
+y = input()
+z = input()
+
+if x == 'vertebrado' and y == 'ave' and z == 'carnivoro':
+    a = 'aguia'
+
+if x == 'vertebrado' and y == 'ave' and z == 'onivoro':
+    a = 'pomba'
+
+if x == 'vertebrado' and y == 'mamifero' and z == 'onivoro':
+    a = 'homem'
+
+if x == 'vertebrado' and y == 'mamifero' and z == 'herbivoro':
+    a = 'vaca'
+
+if x == 'invertebrado' and y == 'inseto' and z == 'hematofago':
+    a = 'pulga'
+
+if x == 'invertebrado' and y == 'inseto' and z == 'herbivoro':
+    a = 'lagarta'
+
+if x == 'invertebrado' and y == 'anelideo' and z == 'hematofago':
+    a = 'sanguessuga'
+
+if x == 'invertebrado' and y == 'anelideo' and z == 'onivoro':
+    a = 'minhoca'
+
+print(a)
+'''
