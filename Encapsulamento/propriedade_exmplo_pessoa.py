bd0ca0492e5addb00b0b858402f88418dddc67b3
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2023-04-14 17:34:31 -0300

    propriedade, encapsulamento

---
+++ Encapsulamento/propriedade_exmplo_pessoa.py
@@ -0,0 +1,17 @@
+class pessoa:
+    def __init__(self, nome, ano_nascimento):
+        self._nome = nome
+        self._ano_nascimento = ano_nascimento
+
+    @property
+    def nome(self):
+        return self._nome
+    
+    @property
+    def idade(self):
+        _ano_atual = 2022
+        return _ano_atual - self._ano_nascimento
+
+
+pessoa = pessoa("Jacque", 1996)
+print(f"Nome: {pessoa.nome} \tIdade: {pessoa.idade}")
